087efc97ecfe3f0d6a450c20d41d936e74738d67
Marcin <marcinkrueger@onet.eu>
2020-07-29 14:27:12 +0200

    Add info section and table create function

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 var txtAreaDocument = document.querySelector('.text');
 var emojiAreaDocument = document.querySelector('.emoji');
 var button = document.querySelector('button.copy');
+var table = document.querySelector('.alphabet table');
 
 txtAreaDocument.addEventListener("focus", (event) => {event.target.className = "notext"})
 txtAreaDocument.addEventListener("focusout", (event) => {if(event.target.value == ""){event.target.className = "text"}})
 txtAreaDocument.addEventListener("input", translate)
 button.addEventListener("click", () => {emojiAreaDocument.select();document.execCommand('copy')})
 
+/*
+    '0': '0⃣',
+    '1': '1⃣',
+    '2': '2⃣',
+    '3': '3⃣',
+    '4': '4⃣',
+    '5': '5⃣',
+    '6': '6⃣',
+    '7': '7⃣',
+    '8': '8⃣',
+    '9': '9⃣',
+*/
 dictionary = {
-    '1': String.fromCodePoint(0xFE0F),
+    '?': '❓',
+    '!': '❗',
     'a': '🍉',
     'b': '🎟️',
     'c': '🍋',
@@ -26,7 +40,7 @@ dictionary = {
     'm': '🐜',
     'n': '👃',
     'o': '👓',
-    'ó': '8️⃣',
+    'ó': '🎱',
     'p': '🐧',
     'r': '🚀',
     's': '🎻',
@@ -52,3 +66,26 @@ function translateWord(str){
 function translate(e){
     emojiAreaDocument.textContent = translateWord(e.target.value)
 }
+
+function makeTable(dict, tab){
+    i = 0;
+    for(const prop in dict){
+        if(prop === ' ')
+            break;
+        if(i % 2 == 0){
+            var tr = document.createElement("tr");
+            table.appendChild(tr);
+        }
+        var td1 = document.createElement("td");
+        var td1_text = document.createTextNode(prop.toUpperCase());
+        td1.appendChild(td1_text);
+        var td2 = document.createElement("td");
+        var td2_text = document.createTextNode(dict[prop]);
+        td2.appendChild(td2_text);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        i++;
+    }
+}
+
+makeTable(dictionary,table);
